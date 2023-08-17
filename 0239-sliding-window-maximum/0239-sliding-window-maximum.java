ffdd0bd55091fc885bdf20f84e743aa9b915cb97
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2023-08-17 10:31:28 -0400

    Time: 31 ms (82.01%), Space: 59.4 MB (70.39%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,5 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+
+    int[] result = new int[nums.length-k+1];
+    int left = 0;
+    Deque<Integer> q = new LinkedList<>();
+
+    for (int right = 0; right < nums.length; right++) {
+        // Remove indices that are out of the current window
+        while (!q.isEmpty() && q.peekFirst() < right - k + 1) {
+            q.pollFirst();
+        }
+
+        // Remove indices whose corresponding values are less than current element
+        while (!q.isEmpty() && nums[q.peekLast()] < nums[right]) {
+            q.pollLast();
+        }
+
+        // Add the current element's index
+        q.offer(right);
+
+        // If we've at least seen k elements, add the current max to result
+        if (right >= k - 1) {
+            result[right - k + 1] = nums[q.peekFirst()];
+        }
+    }
+
+    return result;
+
+
     //     int[] result = new int[nums.length-k+1];
     //     int left = 0;
     //     int max = Integer.MIN_VALUE;
@@ -31,26 +59,7 @@ class Solution {
     //     return max;
     // }
 
-    int[] result = new int[nums.length-k+1];
-    int left = 0;
-    Deque<Integer> q = new LinkedList<>();
 
-    for(int right = 0; right<nums.length; right++) {
-
-        // 
-        if(!q.isEmpty() && q.peekFirst() < right-k+1 )
-            q.pollFirst();
-        
-        while(!q.isEmpty() && nums[right] > nums[q.peekLast()])
-            q.pollLast();
-
-        q.offer(right);
-        
-        if(right >= k-1)
-            result[left++] = nums[q.peekFirst()];
-    }
-
-    return result;
 
 }
 }
