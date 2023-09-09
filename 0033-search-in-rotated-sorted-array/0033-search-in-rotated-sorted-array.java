513928759c8fad9e52fbddcd626167fab9f80038
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2023-09-09 11:43:30 -0400

    Time: 0 ms (100%), Space: 41 MB (42.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,32 +1,64 @@
 class Solution {
     public int search(int[] nums, int target) {
+        // int low = 0;
+        // int high = nums.length-1;
+
+        // while(low <= high) {
+        //     int mid = (low + high) /2 ;
+
+        //     if(nums[mid] == target) {
+        //         return mid;
+        //     }
+        //     if(nums[low] <= nums[mid]) { // Left half is sorted.
+        //         if(nums[low] <= target && target <= nums[mid]) {
+        //             high = mid-1;
+        //         }
+        //         else {
+        //             low = mid+1;
+        //         }
+        //     }
+        //     else if(nums[low]>=nums[mid]){ // Right half is sorted.
+        //         if(nums[mid] <= target && target <= nums[high]) {
+        //             low = mid + 1;
+        //         }
+        //         else {
+        //             high = mid-1;
+        //         }
+        //     }
+        //     else {
+        //         low ++;
+        //     }
+        // }
+
+        // return -1;
+
         int low = 0;
         int high = nums.length-1;
 
         while(low <= high) {
-            int mid = (low + high) /2 ;
+            int mid = low + (high - low)/2;
 
             if(nums[mid] == target) {
                 return mid;
             }
-            if(nums[low] <= nums[mid]) { // Left half is sorted.
-                if(nums[low] <= target && target <= nums[mid]) {
+
+            if(nums[mid] > nums[high]) {
+                // right half is smaller than the left.
+                if(target >= nums[low] && target < nums[mid]) {
                     high = mid-1;
                 }
                 else {
                     low = mid+1;
                 }
             }
-            else if(nums[low]>=nums[mid]){ // Right half is sorted.
-                if(nums[mid] <= target && target <= nums[high]) {
-                    low = mid + 1;
+            else {
+                // left half is smaller than the right.
+                if(target > nums[mid] && target <= nums[high]) {
+                    low = mid+1;
                 }
                 else {
                     high = mid-1;
                 }
-            }
-            else {
-                low ++;
             }
         }
 
