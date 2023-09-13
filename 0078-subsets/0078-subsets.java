2139701ef35b99fff02e5bc4782efe6092308011
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2023-09-13 07:53:03 -0400

    Time: 1 ms (71.4%), Space: 42 MB (31.69%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,22 +2,33 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<Integer> list = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
-
+        // Arrays.sort(nums);
         backtrack(0, nums, list, result);        
         return result;   
     }
 
+    // private void backtrack(int index, int[] nums, List<Integer> list, List<List<Integer>> result) {
+    //     if(index >= nums.length) {
+    //         result.add(new ArrayList<>(list));
+    //         return;
+    //     }
+    //     list.add(nums[index]);
+    //     backtrack(index + 1, nums, list, result);
+    //     list.remove(list.size()-1);
+    //     backtrack(index + 1, nums, list, result);
+        
+    // }
+
     private void backtrack(int index, int[] nums, List<Integer> list, List<List<Integer>> result) {
-        if(index >= nums.length) {
-            result.add(new ArrayList<>(list));
-            return;
+
+        result.add(new ArrayList(list));
+
+        for(int i=index; i<nums.length; i++) {
+            list.add(nums[i]);
+            backtrack(i+1, nums, list, result);
+            list.remove(list.size()-1);
         }
-        list.add(nums[index]);
-        backtrack(index + 1, nums, list, result);
-        list.remove(list.size()-1);
-        backtrack(index + 1, nums, list, result);
         
     }
-
 
 }
