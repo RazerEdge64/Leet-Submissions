0be53c41af529c3250f193dd78a51e655fbdfe60
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2023-09-13 07:47:11 -0400

    Time: 2 ms (34.94%), Space: 43.4 MB (92.28%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,11 +1,11 @@
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        subsetHelper(0, nums, list, ans);
-        return ans;
+    // public List<List<Integer>> subsetsWithDup(int[] nums) {
+    //     List<Integer> list = new ArrayList<>();
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     subsetHelper(0, nums, list, ans);
+    //     return ans;
 
-    }
+    // }
 
     // public static void subsetHelper(int index, int[] nums, List<Integer> list, List<List<Integer>> ans) {
     //     if(index == nums.length) {
@@ -21,21 +21,23 @@ class Solution {
     //     list.remove(list.size()-1);
     //     subsetHelper(index+1, nums, list, ans);
     // }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        subsetHelper(0, nums, list, ans);
+        return ans;
 
-public static void subsetHelper(int index, int[] nums, List<Integer> list,  List<List<Integer>> ans) {
+    }    
 
-    if(index == nums.length) {
-        List<Integer> temp = new ArrayList(list);
-        Collections.sort(temp);
-        if(!ans.contains(temp)) {
-            ans.add(temp);
+    public static void subsetHelper(int index, int[] nums, List<Integer> list,  List<List<Integer>> ans) {
+        ans.add(new ArrayList(list));
+
+        for(int idx = index; idx<nums.length; idx++) {
+            if(idx > index && nums[idx] == nums[idx-1]) continue;
+            list.add(nums[idx]);
+            subsetHelper(idx+1, nums, list, ans);
+            list.remove(list.size()-1);
         }
-        return;
     }
-
-    list.add(nums[index]);
-    subsetHelper(index+1, nums, list, ans);
-    list.remove(list.size()-1);
-    subsetHelper(index+1, nums, list, ans);
-}
 }
