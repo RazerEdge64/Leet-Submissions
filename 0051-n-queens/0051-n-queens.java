bd907574481e020b6d87f80cd2922114e9750715
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2023-09-15 19:02:19 -0400

    Time: 1 ms (100%), Space: 44.3 MB (13.15%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,70 +1,117 @@
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
+    // public List<List<String>> solveNQueens(int n) {
+    //     char[][] board = new char[n][n];
+    //     for(int i=0; i<n; i++) {
+    //         for(int j=0; j<n; j++) {
+    //             board[i][j] = '.';
+    //         }
+    //     }
+
+    //     List<List<String>> res = new ArrayList<>();
+    //     backtrack(0, board, res);
+
+    //     return res;
+    // }
+
+    // private void backtrack(int col, char[][]board, List<List<String>> res) {
+    //     if(col == board.length) {
+    //         res.add(construct(board));
+    //         return;
+    //     }
+
+    //     for(int row=0; row<board.length; row++) {
+    //         if(isSafe(row, col, board)) {
+    //             board[row][col] = 'Q';
+    //             backtrack(col+1, board, res);
+    //             board[row][col] = '.';
+    //         }
+    //     }
+
+    // }
+
+    // private boolean isSafe(int row, int col, char[][] board) {
+    //     // upper diagonal.
+    //     int dummyRow = row;
+    //     int dummyCol = col;
+    //     while(col>=0 && row>=0) {
+    //         if(board[row][col] == 'Q') return false;
+    //         row--;
+    //         col--;
+    //     }
+    //     row = dummyRow;
+    //     col = dummyCol;
+
+    //     // same row.
+    //     while(col>=0) {
+    //         if(board[row][col] == 'Q') return false;
+    //         col--;
+    //     }
+    //     row = dummyRow;
+    //     col = dummyCol;
+    //     // lower diagonal
+
+    //     while(row<board.length && col>=0) {
+    //         if(board[row][col] == 'Q') return false;
+    //         row++;
+    //         col--;
+    //     }
+
+    //     return true;
+    // }
+
+    // private List<String> construct(char[][] board) {
+    //     List < String > res = new LinkedList < String > ();
+    //     for(int i=0; i<board.length; i++) {
+    //         String s = new String(board[i]);
+    //         res.add(s);
+    //     }
+    //     return res;
+    // }
+
+    public static List < List < String >> solveNQueens(int n) {
         char[][] board = new char[n][n];
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 board[i][j] = '.';
-            }
-        }
-
-        List<List<String>> res = new ArrayList<>();
-        backtrack(0, board, res);
-
+        List < List < String >> res = new ArrayList < List < String >> ();
+        int leftRow[] = new int[n];
+        int upperDiagonal[] = new int[2 * n - 1];
+        int lowerDiagonal[] = new int[2 * n - 1];
+        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
         return res;
     }
 
-    private void backtrack(int col, char[][]board, List<List<String>> res) {
-        if(col == board.length) {
+
+
+    static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
+        if (col == board.length) {
             res.add(construct(board));
             return;
         }
 
-        for(int row=0; row<board.length; row++) {
-            if(isSafe(row, col, board)) {
+        for (int row = 0; row < board.length; row++) {
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
                 board[row][col] = 'Q';
-                backtrack(col+1, board, res);
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
                 board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
             }
         }
-
     }
 
-    private boolean isSafe(int row, int col, char[][] board) {
-        // upper diagonal.
-        int dummyRow = row;
-        int dummyCol = col;
-        while(col>=0 && row>=0) {
-            if(board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-        row = dummyRow;
-        col = dummyCol;
 
-        // same row.
-        while(col>=0) {
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-        row = dummyRow;
-        col = dummyCol;
-        // lower diagonal
-
-        while(row<board.length && col>=0) {
-            if(board[row][col] == 'Q') return false;
-            row++;
-            col--;
-        }
-
-        return true;
-    }
-
-    private List<String> construct(char[][] board) {
+    static List < String > construct(char[][] board) {
         List < String > res = new LinkedList < String > ();
-        for(int i=0; i<board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
             String s = new String(board[i]);
             res.add(s);
         }
         return res;
     }
+
 }
