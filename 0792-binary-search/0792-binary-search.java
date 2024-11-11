074d61986028a5eeb7d534190e6517656f0071fa
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2024-11-11 15:45:15 -0500

    Time: 0 ms (100%), Space: 45.6 MB (64.01%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,21 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start=0;
         int end = nums.length-1;
+        int start = 0;
 
-        while(start<=end) {
+        while(start <= end) {
             int mid = start + (end-start)/2;
-            if(target == nums[mid]) {
+            if(nums[mid] == target) {
                 return mid;
             }
 
-            if(target < nums[mid]) {
-                end = mid-1;
-            }
-            else if(target > nums[mid]) {
+            if(target>nums[mid]) {
                 start = mid+1;
             }
+            else if(target<nums[mid]) {
+                end = mid-1;
+            }
         }
+
         return -1;
     }
 }
