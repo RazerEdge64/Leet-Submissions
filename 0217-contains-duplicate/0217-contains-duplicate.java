69032e979ca80326ce44b1c605714ef4d30b6d65
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-03-07 15:15:29 -0500

    Time: 20 ms (23.12%), Space: 55.5 MB (80.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        if(nums.length < 2) {
+            return false;
+        }
+        if(nums.length == 2) {
+            return nums[0] == nums[1];
+        }
+
         Arrays.sort(nums);
         for(int i=0; i<nums.length-1; i++) {
-            if(nums[i] == nums[i+1]) {
+            if(nums[i]==nums[i+1]) {
                 return true;
             }
         }
-        return false;
+
+        return false; 
     }
 }
