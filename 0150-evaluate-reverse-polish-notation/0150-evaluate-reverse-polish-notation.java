cbeb178bbeb1a48a64f155d68631b6d6c3cedd99
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-03-08 11:01:46 -0500

    Time: 5 ms (97.76%), Space: 45 MB (22.56%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,29 +1,28 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<String> stack = new Stack<>();
-        for(int i=0; i<tokens.length; i++) {
-            String token = tokens[i];
-            if(token.equals("+") || token.equals("-") || token.equals("/") || token.equals("*")) {
-                String token1 = stack.pop();
-                String token2 = stack.pop();
-                int result = 0;
-                if(token.equals("+")) {
-                    result = Integer.parseInt(token1) + Integer.parseInt(token2);
-                }
-                if(token.equals("-")) {
-                    result = Integer.parseInt(token2) - Integer.parseInt(token1);
-                }
-                if(token.equals("*")) {
-                    result = Integer.parseInt(token2) * Integer.parseInt(token1);
-                }
-                if(token.equals("/")) {
-                    result = Integer.parseInt(token2) / Integer.parseInt(token1);
-                }
-                stack.push(result+"");
-                continue;
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    int b1 = stack.pop(), a1 = stack.pop();
+                    stack.push(a1 - b1);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int b2 = stack.pop(), a2 = stack.pop();
+                    stack.push(a2 / b2);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
             }
-            stack.push(token);
         }
-        return Integer.parseInt(stack.peek());
+        return stack.pop();
+
     }
 }
