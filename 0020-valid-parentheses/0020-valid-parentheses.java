29543c1714ac80806ab94a5c3c106c2f394c3bde
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-03-08 09:37:14 -0500

    Time: 3 ms (42.17%), Space: 42.1 MB (13.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,37 +1,32 @@
 class Solution {
 
   public boolean isValid(String s) {
-    Stack<String> charStack = new Stack<String>();
-
-    for (int i = 0; i < s.length(); i++) {
-
-      if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-        charStack.push(s.charAt(i) + "");
-        continue;
-      } else if (charStack.empty()) {
-        charStack.push(s.charAt(i)+"");
+    
+      Stack<Character> stack = new Stack<>();
+      
+      for(int i=0; i< s.length(); i++) {
+          
+          Character c = s.charAt(i);
+          if(!stack.empty()) {
+            if(c == ')' && stack.peek() == '(' ) {
+              stack.pop();
+            } else if( c == ']' && stack.peek() == '[') {
+              stack.pop();
+            } else if (c == '}' && stack.peek() == '{' ) {
+               stack.pop();
+            }
+              else {
+                  stack.push(c);
+                  continue;
+              }
+          }
+            else {
+                stack.push(c);        
+              }
+          
       }
-      else{
-        if (s.charAt(i) == ')' && charStack.peek().equals("(")) { //||  ||
-          charStack.pop();
-          continue;
-        }
-
-        if (s.charAt(i) == '}' && charStack.peek().equals("{")) {
-          charStack.pop();
-          continue;
-        }
-        if (s.charAt(i) == ']' && charStack.peek().equals("[")) {
-          charStack.pop();
-          continue;
-        }
-        charStack.push(s.charAt(i)+"");
-      }
-
-    }
-
-
-    return charStack.empty();
+      
+      return stack.empty();
   }
 
 }
