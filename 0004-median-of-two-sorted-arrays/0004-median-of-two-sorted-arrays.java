c23d3d42d40e3f1fcf32b07d87928a8046a678e6
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-03-10 18:30:13 -0400

    Time: 2 ms (58.81%), Space: 46.3 MB (43.22%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,46 +1,31 @@
 class Solution {
+    private int p1 = 0, p2 = 0;
+
+    // Get the smaller value between nums1[p1] and nums2[p2] and move the pointer forwards.
+
+    private int getMin(int[] nums1, int[] nums2) {
+        if (p1 < nums1.length && p2 < nums2.length) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < nums1.length) {
+            return nums1[p1++];
+        } else if (p2 < nums2.length) {
+            return nums2[p2++];
+        }
+        return -1;
+    }
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i=0, j=0, x=0;
-        int n = nums1.length;
-        int m = nums2.length;
-        int median = (n+m)/2;
-        int[] arr = new int[n+m];
-
-        while(i<n && j<m) {
-            if(nums1[i] < nums2[j]) {
-                System.out.println(nums1[i]+" "+nums2[j]);
-                arr[x] = nums1[i];
-                i++;
-                x++;
+        int m = nums1.length, n = nums2.length;
+        if ((m + n) % 2 == 0) {
+            for (int i = 0; i < (m + n) / 2 - 1; ++i) {
+                int tmp = getMin(nums1, nums2);
             }
-            else {
-                System.out.println("Hi "+nums2[j]);
-                arr[x] = nums2[j];
-                j++;
-                x++;
+            return (double) (getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
+        } else {
+            for (int i = 0; i < (m + n) / 2; ++i) {
+                int tmp = getMin(nums1, nums2);
             }
+            return getMin(nums1, nums2);
         }
-        while(i<n) {
-            arr[x] = nums1[i];
-            i++;
-            x++;
-        }
-        while(j<m) {
-            arr[x] = nums2[j];
-            j++;
-            x++;
-        }
-        System.out.println("Median "+median);
-
-        for(int z=0 ; z<m+n ; z++) {
-            System.out.println(arr[z]);
-        }
-
-        if((n+m) % 2 ==0){
-            return (arr[median-1] + arr[median])/2.0;
-        }
-        return arr[median];
-        
-        
     }
 }
