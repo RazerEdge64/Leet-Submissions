47cbda64f86cf34e8a5ae9b1da4c4a5e07ca1e9c
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-03-30 10:02:31 -0400

    Time: 31 ms (67.23%), Space: 65.3 MB (5.45%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,65 +1,27 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+       if(nums == null || k==0) return new int[0];
 
-    int[] result = new int[nums.length-k+1];
-    int left = 0;
-    Deque<Integer> q = new LinkedList<>();
+       int n = nums.length;
+       int[] result = new int[n-k + 1];
+       Deque<Integer> deque = new LinkedList<>();
 
-    for (int right = 0; right < nums.length; right++) {
-        // Remove indices that are out of the current window
-        while (!q.isEmpty() && q.peekFirst() < right - k + 1) {
-            q.pollFirst();
-        }
+       for(int right = 0; right<n; right++) {
+            while(!deque.isEmpty() && deque.peekFirst() < right-k+1) {
+                deque.pollFirst();
+            }
 
-        // Remove indices whose corresponding values are less than current element
-        while (!q.isEmpty() && nums[q.peekLast()] < nums[right]) {
-            q.pollLast();
-        }
+            while(!deque.isEmpty() && nums[deque.peekLast()] < nums[right]) {
+                deque.pollLast();
+            }
 
-        // Add the current element's index
-        q.offer(right);
+            deque.offerLast(right);
 
-        // If we've at least seen k elements, add the current max to result
-        if (right >= k - 1) {
-            result[right - k + 1] = nums[q.peekFirst()];
-        }
+            if(right >= k-1) {
+                result[right-k+1] = nums[deque.peekFirst()];
+            }
+       }
+
+       return result;
     }
-
-    return result;
-
-
-    //     int[] result = new int[nums.length-k+1];
-    //     int left = 0;
-    //     int max = Integer.MIN_VALUE;
-    //     for(int right = 0; right<nums.length; right++) {
-    //         // max = Math.max(max, nums[right]);
-            
-    //         if(right>=k-1) {
-    //             // if(nums[left]==max) {
-    //             System.out.println(left+" "+right);
-    //             max = nextMax(left, right, nums);
-    //             // }
-    //             result[left] = max;
-    //             left++;
-    //         }
-            
-    //     }
-
-    //     return result;
-    // }
-
-    // private int nextMax(int a, int b, int[] nums) {
-    //     int max = Integer.MIN_VALUE;
-    //     for(int i=a; i<=b; i++) {
-    //         if(nums[i]>max){
-    //             max = nums[i];
-    //         }
-    //     }
-
-    //     return max;
-    // }
-
-
-
-}
 }
