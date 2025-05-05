8f83ae81540530ae95865e80d1cd3b4dbca4660e
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-05 15:23:21 -0400

    Time: 1 ms (99.93%), Space: 43.5 MB (78.04%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,43 +1,20 @@
 class Solution {
-    // public List<List<Integer>> subsetsWithDup(int[] nums) {
-    //     List<Integer> list = new ArrayList<>();
-    //     List<List<Integer>> ans = new ArrayList<>();
-    //     subsetHelper(0, nums, list, ans);
-    //     return ans;
-
-    // }
-
-    // public static void subsetHelper(int index, int[] nums, List<Integer> list, List<List<Integer>> ans) {
-    //     if(index == nums.length) {
-    //         List<Integer> temp = new ArrayList<>(list);
-    //         Collections.sort(temp);
-    //         if(!ans.contains(temp))
-    //             ans.add(temp);
-    //         return;
-    //     }
-
-    //     list.add(nums[index]);
-    //     subsetHelper(index+1, nums, list, ans);
-    //     list.remove(list.size()-1);
-    //     subsetHelper(index+1, nums, list, ans);
-    // }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums.length == 0) return result;
         Arrays.sort(nums);
-        subsetHelper(0, nums, list, ans);
-        return ans;
-
-    }    
-
-    public static void subsetHelper(int index, int[] nums, List<Integer> list,  List<List<Integer>> ans) {
-        ans.add(new ArrayList(list));
-
-        for(int idx = index; idx<nums.length; idx++) {
-            if(idx > index && nums[idx] == nums[idx-1]) continue;
-            list.add(nums[idx]);
-            subsetHelper(idx+1, nums, list, ans);
-            list.remove(list.size()-1);
-        }
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
     }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> path, int[] nums, int index) {
+        result.add(new ArrayList<>(path));
+
+        for(int i = index; i<nums.length; i++) {
+            if(i > index && nums[i] == nums[i-1]) continue;
+            path.add(nums[i]);
+            backtrack(result, path, nums, i+1);
+            path.remove(path.size() - 1);
+        }
+    } 
 }
