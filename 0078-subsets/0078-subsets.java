7ae8c211791a7fae852b04ecb577f1f540dc3cf2
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-05 15:17:13 -0400

    Time: 0 ms (100%), Space: 42.8 MB (46.79%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,34 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> list = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
-        // Arrays.sort(nums);
-        backtrack(0, nums, list, result);        
-        return result;   
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
     }
 
-    // private void backtrack(int index, int[] nums, List<Integer> list, List<List<Integer>> result) {
-    //     if(index >= nums.length) {
-    //         result.add(new ArrayList<>(list));
-    //         return;
-    //     }
-    //     list.add(nums[index]);
-    //     backtrack(index + 1, nums, list, result);
-    //     list.remove(list.size()-1);
-    //     backtrack(index + 1, nums, list, result);
-        
-    // }
+    private void backtrack(List<List<Integer>> result, List<Integer> path, int nums[], int index) {
+        result.add(new ArrayList<>(path));
 
-    private void backtrack(int index, int[] nums, List<Integer> list, List<List<Integer>> result) {
-
-        result.add(new ArrayList(list));
-
-        for(int i=index; i<nums.length; i++) {
-            list.add(nums[i]);
-            backtrack(i+1, nums, list, result);
-            list.remove(list.size()-1);
+        for(int i = index; i<nums.length; i++) {
+            path.add(nums[i]);
+            backtrack(result, path, nums, i+1);
+            path.remove(path.size()-1);
         }
         
     }
-
 }
