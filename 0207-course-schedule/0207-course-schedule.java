e8effdbeaa41ef7f9fe66878c60cce56d5571f07
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-09 15:07:54 -0400

    Time: 4 ms (90.18%), Space: 45.1 MB (83.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,42 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] graph = new ArrayList[numCourses];
-        int[] inDegree = new int[numCourses];
-
-        // Intializing the graph
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i=0; i<numCourses; i++) {
-            graph[i] = new ArrayList<>();
+            graph.add(new ArrayList<>());
         }
 
-        // Build the graph
-        for(int[] pair : prerequisites) {
-            graph[pair[1]].add(pair[0]);
-            inDegree[pair[0]]++;
+        for(int[] pair: prerequisites) {
+            graph.get(pair[1]).add(pair[0]);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[numCourses];
+        boolean[] onPath = new boolean[numCourses];
 
-        for(int i=0; i< numCourses; i++) {
-            if(inDegree[i] == 0) {
-                queue.add(i);
-            }
-        }
-
-        int visitedNodes = 0;
-
-        while(!queue.isEmpty()) {
-            int current =  queue.poll();
-            visitedNodes++;
-
-            for(int neighbor : graph[current]) {
-                inDegree[neighbor]--;
-                if(inDegree[neighbor] == 0) {
-                    queue.add(neighbor);
+        for(int i=0; i<numCourses; i++) {
+            if(!visited[i]) {
+                if(hasCycle(graph, i, visited, onPath)) {
+                    return false;
                 }
             }
         }
 
-        return visitedNodes == numCourses;
+        return true;
+    }
 
+    private boolean hasCycle(List<List<Integer>> graph, int node, boolean[] visited, boolean[] onPath) {
+        visited[node] = true;
+        onPath[node] = true;
 
+        for(int neighbor: graph.get(node)) {
+            if(!visited[neighbor]) {
+                if( hasCycle(graph, neighbor, visited, onPath)) return true;
+            } else if (onPath[neighbor]) {
+                return true;
+            }
+        }
 
+        onPath[node] = false;
 
-     }
+        return false;
+    }
 }
