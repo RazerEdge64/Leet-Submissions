a53922459f7b17f55ea6aaeb342aace948a34322
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-16 16:18:10 -0400

    Time: 145 ms (57.85%), Space: 41.9 MB (39.65%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,32 +1,34 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                if(backtrack(i, j, board, word, 0))
+        boolean[][] used = new boolean[board.length][board[0].length];
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                if((word.charAt(0) == board[i][j]) && dfs(board, used, word, i, j, 0) ) {
                     return true;
+                }
             }
         }
         return false;
-
     }
-
-    private boolean backtrack(int i, int j, char[][] board, String word, int idx) {
-        if(idx == word.length())
+    
+    private boolean dfs(char[][] board, boolean[][] used, String word, int i, int j, int index) {
+        if(index == word.length()) {
             return true;
+        }
 
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx))
-            return false;
+        if((i<0 || j<0 || i>= board.length || j >= board[0].length || word.charAt(index) != board[i][j])) return false;
+        
+        if(used[i][j]) return false;
 
-        char temp = board[i][j];
-        board[i][j] = '#'; // Mark cell as visited.
+        used[i][j] = true;
+        boolean found =  dfs(board, used, word, i+1, j, index+1) || 
+                dfs(board, used, word, i-1, j, index+1) || 
+                dfs(board, used, word, i, j+1, index+1) || 
+                dfs(board, used, word, i, j-1, index+1);
 
-        boolean found = backtrack(i+1, j, board, word, idx+1) ||
-                        backtrack(i-1, j, board, word, idx+1) ||
-                        backtrack(i, j+1, board, word, idx+1) ||
-                        backtrack(i, j-1, board, word, idx+1);
-
-        board[i][j] = temp; // revert the change (backtrack).
+        used[i][j] = false;
 
         return found;
     }
+
 }
