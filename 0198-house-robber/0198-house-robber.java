090039fcf5ae53ddcd0fa502ed7132ad5b8aab20
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-16 11:09:01 -0400

    Time: 0 ms (100%), Space: 41 MB (60.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int rob1=0, rob2 = 0;
+        // rob1, rob2, n, n+1 ...
+        int rob1 = 0;
+        int rob2 = 0;
 
         for(int i=0; i<n; i++) {
-            int curr = Math.max(nums[i] + rob1, rob2);
+            int temp = Math.max(rob1 + nums[i], rob2);
             rob1 = rob2;
-            rob2 = curr;
+            rob2 = temp;
         }
 
         return rob2;
