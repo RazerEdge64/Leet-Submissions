915607459558593e98e52648440b7a47c5a1cdeb
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-16 15:32:16 -0400

    Time: 1 ms (51.39%), Space: 43.1 MB (18.05%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         backtrack(result, new ArrayList<>(), nums, 0);
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> path, int nums[], int index) {
-        result.add(new ArrayList<>(path));
-
-        for(int i = index; i<nums.length; i++) {
-            path.add(nums[i]);
-            backtrack(result, path, nums, i+1);
-            path.remove(path.size()-1);
+    private void backtrack(List<List<Integer>> result, List<Integer> path, int[] nums, int index) {
+        if(index == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
         }
-        
+        path.add(nums[index]);
+        backtrack(result, path, nums, index+1);
+        path.remove(path.size()-1);
+        backtrack(result, path, nums, index+1);
     }
 }
