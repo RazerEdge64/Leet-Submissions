042a48ca187c8030a97f280ffcf8e0d92c1f2faa
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-16 20:20:30 -0400

    Time: 2 ms (18.06%), Space: 42.3 MB (17.86%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,26 +1,36 @@
 class Solution {
     public int numDecodings(String s) {
-        int n = s.length();
-        Integer[] mem = new Integer[n];
-        return s.length()==0? 0:numDecodings(0, s, mem);
+        Map<Integer, Integer> memo = new HashMap<>();
+        return backtrack(s, 0, memo);
     }
 
-    private int numDecodings(int p, String s, Integer[] mem) {
-        int n = s.length();
-
-        if(p==n) return 1;
-
-        if(s.charAt(p)=='0') return 0;
-        if(mem[p]!=null) return mem[p];
-
-        int res = numDecodings(p+1, s, mem);
-
-        if(p<n-1 && (s.charAt(p)=='1' || s.charAt(p)=='2' && s.charAt(p+1)<'7') ) {
-            res += numDecodings(p+2, s, mem);
+    private int backtrack(String s, int index, Map<Integer, Integer> memo) {
+        if(index == s.length()) {
+            return 1;
         }
 
-        return mem[p] = res;
+        if(memo.containsKey(index)) return memo.get(index);
+
+        int total = 0;
+        String c = s.substring(index, index+1);
+        if(isValid(c)) {
+            total+= backtrack(s, index+1, memo);
+        }
+
+        if(index+1 < s.length()) {
+            String c2 = s.substring(index, index+2);
+            if(isValid(c2)) {
+                total+= backtrack(s, index+2, memo);
+            }
+        }
+
+        memo.put(index, total);
+        return total;
     }
 
-
+    private boolean isValid(String s) {
+        if(s.charAt(0) == '0') return false;
+        int val = Integer.parseInt(s);
+        return val>=1 && val<=26;
+    }
 }
