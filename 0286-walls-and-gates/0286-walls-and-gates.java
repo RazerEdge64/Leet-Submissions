15864f80601ff4b508274b7277d04f4712ebcecf
Shubhang Kohirkar <44175007+RazerEdge64@users.noreply.github.com>
2025-05-18 14:01:49 -0400

    Time: 12 ms (23.88%), Space: 46.3 MB (98.64%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -8,28 +8,28 @@ class Solution {
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(rooms[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
-                }        
+                    queue.offer(new int[]{i, j});
+                }
             }
         }
-
-        int[][] directions = {{1,0}, {-1, 0}, {0,1}, {0,-1}};
 
         while(!queue.isEmpty()) {
             int[] cell = queue.poll();
             int row = cell[0];
             int col = cell[1];
 
-            for(int[] direction: directions) {
-                int r = row + direction[0];
-                int c = col + direction[1];
+            int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+            for(int[] dir : directions) {
+                int r = row + dir[0];
+                int c = col + dir[1];
 
                 if(r<0 || c<0 || r>=m || c>=n || rooms[r][c] != Integer.MAX_VALUE) continue;
 
-                rooms[r][c] = rooms[row][col] +1;
+                rooms[r][c] = rooms[row][col] + 1;
                 queue.offer(new int[]{r, c});
             }
-
         }
+
+        return;
     }
 }
